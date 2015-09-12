c21cc0507a904ba9b14b242cd4f28b456cc40c5c
Masaki Kagaya <masakielastic@gmail.com>
2015-09-13 08:05:16 +0900

    ES class style

--- view/app/app.jsx
+++ view/app/app.jsx
@@ -1,7 +1,10 @@
-var MyComponent = React.createClass({
-  render: function() {
+class MyComponent extends React.Component {
+  render() {
     return <div>Hello World</div>;
   }
-});
+}
 
-ReactDOM.render(<MyComponent />, document.getElementById('container'));
+ReactDOM.render(
+    <MyComponent />,
+    document.getElementById('container')
+);
